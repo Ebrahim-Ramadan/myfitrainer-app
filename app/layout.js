5946b3b77071fd04fc1b60b4e0be7050157bfb4c
ebrahim-ramadan <ramadanebrahim791@gmail.com>
2023-11-11 20:11:35 +0200

    fixed production typos

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,6 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { Header } from '@/components/globals/Header'
-import { AuthProvider } from "@/context/AuthProvider";
 
 const inter = Inter({ subsets: ['latin'] })
 
