2b6ff14d011c46b45308c086d388ebbd23787dc7
ebrahim-ramadan <ramadanebrahim791@gmail.com>
2023-11-15 21:45:17 +0200

    added env variables on vercel production concole

--- components/progress/RoutinesMap.jsx
+++ components/progress/RoutinesMap.jsx
@@ -8,6 +8,7 @@ console.log('exercises', exercises);
   return (
     <div className='px-2 rounded-lg border border-2 border-slate-400'>
       <div className='font-bold text-gray-400 mt-2 underline'>{data.muscle}</div>
+      
       <ul className='flex flex-col'>
         {exercises.map((routine) => (
           <MuscleModal key={routine.name} name={routine.name} muscle={routine.muscle} instruciton={routine.instruciton} />
